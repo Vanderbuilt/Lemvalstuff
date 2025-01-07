bb022d9c2a7b6c89da968955691a06b3ed8cf677
Vanderbuilt <186883688+Vanderbuilt@users.noreply.github.com>
2025-01-07 11:53:13 -0700

    Update getValStats.sh
    switched startTime to runTime for more clarity

--- prometheus/getValStats.sh
+++ prometheus/getValStats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script: getValStats.sh - A script to gether Lemon Validator statistics
-# Version 1.03
+# Version 1.04
  
 # Options
 # -p, Print out statistics using prometheus formatting
@@ -22,7 +22,7 @@ rewards=$($operaCMD "sfcc.pendingRewards(\"$walletAddr\",$valID);")/$particle
 stake=$($operaCMD "sfcc.getStake(\"$walletAddr\",$valID);")/$particle
 lockedStake=$($operaCMD "sfcc.getLockedStake(\"$walletAddr\",$valID);")/$particle
 delegated=$($operaCMD "sfcc.getValidator($valID)[3];")/$particle
-startTime=$($operaCMD "sfcc.getValidator($valID)[5];")
+runTime=$($operaCMD "sfcc.getValidator($valID)[5];")
 block=$($operaCMD 'ftm.blockNumber;')
 epoch=$($operaCMD 'admin.nodeInfo.protocols.opera.epoch;')
 listening=$($operaCMD 'net.listening;')
@@ -42,7 +42,7 @@ print_stats() {
     echo "Wallet Status: $walletStatus"
     echo "TX Pool Pending: $txPoolPending"
     echo "TX Pool Queued:  $txPoolQueued"
-    echo "Val Start Time:  $startTime"
+    echo "Val Run Time:  $runTime"
     printf "%s" "Staked LEMX: "
     awk "BEGIN {print $stake}" 
     printf "%s" "Locked/Staked LEMX: "
@@ -113,10 +113,10 @@ print_stats_prom() {
     printf "%s" "val_pending_rewards "
     awk "BEGIN {print $rewards}"
 
-    echo "# HELP val_start_time Epoch Time stamp when validator started up"
-    echo "# TYPE val_start_time counter"
-    printf "%s" "val_start_time "
-    awk "BEGIN {print $startTime}"
+    echo "# HELP val_run_time Epoch Time stamp when validator started up"
+    echo "# TYPE val_run_time counter"
+    printf "%s" "val_run_time "
+    awk "BEGIN {print $runTime}"
 
     echo "# HELP val_total_stake Total LEMX staked on the chain"
     echo "# TYPE val_total_stake gauge"
