da00363d7dbf8db49a9a604f12ca93dc989b98d9
Vanderbuilt <186883688+Vanderbuilt@users.noreply.github.com>
2025-01-09 04:28:09 +0000

    cleaned up code
    Updated charts to include Days Running - made a few more minor changes
    
    Updated charts to include Days Running - made a few more minor changes

--- prometheus/getValStats.sh
+++ prometheus/getValStats.sh
@@ -47,24 +47,8 @@ print_stats() {
     echo "Wallet Status: $walletStatus"
     echo "TX Pool Pending: $txPoolPending"
     echo "TX Pool Queued:  $txPoolQueued"
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     printf "%s" "Validator Uptime(days): "
     awk "BEGIN {print $valUpTime}" 
-=======
->>>>>>> 2982578 (fixed minor formatting issue)
-=======
-    printf "%s" "Validator Uptime(days): "
-    awk "BEGIN {print $valUpTime}" 
->>>>>>> c3135d5 (Formatted Validator start time to display in days. Renamed prometheus)
-=======
->>>>>>> 2982578 (fixed minor formatting issue)
-=======
-    printf "%s" "Validator Uptime(days): "
-    awk "BEGIN {print $valUpTime}" 
->>>>>>> c3135d5 (Formatted Validator start time to display in days. Renamed prometheus)
     printf "%s" "Staked LEMX: "
     awk "BEGIN {print $stake}" 
     printf "%s" "Locked/Staked LEMX: "
